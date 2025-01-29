47c80abdd37ff5fb3eaf2762e19ae54d21a841cf
Djiatsa Dunamis Junior <dunamisjunior15@gmail.com>
2025-01-29 16:18:38 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  build: {
-    outDir: 'dist'
-  },
   server: {
     host: true,
     open: true
